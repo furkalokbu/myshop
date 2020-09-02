c694d0d3549bb5c8386a0460d764cc3111a10974
Serhii Furkalo <furkalokbu@gmail.com>
2020-09-02 10:42:22 +0300

    cart add

--- cart/cart.py
+++ cart/cart.py
@@ -11,6 +11,26 @@ class Cart(object):
             cart = self.session[settings.CART_SESSION_ID]={}
         self.cart = cart
 
+    def __iter__(self):
+        """ Проходим по товарам корзины и получаем соответствующие объекты Product """
+        product_ids = self.cart.keys()
+        """ Передаем их в корзину """
+        products = Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+        
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+        """ Сумма товаров в корзине """
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+
     def add(self, product, quantity=1, update_quantity=False):
         product_id = str(product.id)
         if product_id not in self.cart:
@@ -25,30 +45,11 @@ class Cart(object):
         self.session.modified = True
 
     def remove(self, product):
-        producrt_id = str(product.id)
+        product_id = str(product.id)
         if product_id in self.cart:
-            del self.cart[producrt_id]
+            del self.cart[product_id]
             self.save()
     
-    def __iter__(self):
-        """ Проходим по товарам корзины и получаем соответствующие объекты Product """
-        product_ids = self.cart.keys()
-        """ Передаем их в корзину """
-        products = Product.objects.filter(id__in=product_ids)
-
-        cart = self.cart.copy()
-        
-        for product in products:
-            cart[str(product.id)]['product'] = product
-
-        for item in cart.values:
-            item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['quantity']
-        yield item
-
-        """ Сумма товаров в корзине """
-    def __len__(self):
-        return sum(item['quantity'] for imem in self.cart.values())
 
         """ Подсчет общей стоимости  """
     def get_total_price(self):
